e267b964203623a08240ba0f3c365d507305b220
cbedroid <cbedroid1614@gmail.com>
2020-08-24 01:50:07 -0400

    Fixed boostrap navbar not collapsing by using Jquery

--- static/js/main.js
+++ static/js/main.js
@@ -1,47 +1,51 @@
 var test = [];
 $(document).ready(() => {
+  $("body").on("click", () => {
+    $(".navbar-collapse").collapse("hide");
+  });
+  //Fixed bootstrap 4 collaspe not working on mobile
+  let is_open = false;
+  $(".navbar-toggler").on("click", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    const nb = $(".navbar-collapse");
+    const display = { false: "block", true: "none" }[is_open];
+    $(nb).css("display", display);
+    $(this).attr("aria-expanded", is_open == false ? true : false);
+    is_open = !is_open;
+  });
 
-  function showComingSoon () {
+  function showComingSoon() {
     // show modal base on datetime of user last visit
 
-    const cs_modal = $('#modal_upcoming');
+    const cs_modal = $("#modal_upcoming");
     const data = {};
     const vph = 1000 * 60 * 60;
     //* 60; // visit per hour
     const today = new Date().getTime();
-    let last_shown = localStorage.getItem('last_shown');
+    let last_shown = localStorage.getItem("last_shown");
     let time_shown;
     test.push(today, last_shown, vph);
     let show = false;
 
-    if (!last_shown)
-    {
-      localStorage.setItem('last_shown', today);
+    if (!last_shown) {
+      localStorage.setItem("last_shown", today);
       last_shown = today;
-    } else
-    {
+    } else {
       time_shown = today - parseInt(last_shown);
-      time_shown >= vph ? show = true : show = false
+      time_shown >= vph ? (show = true) : (show = false);
     }
 
-    if (show === true)
-    {
+    if (show === true) {
       // set timeout 5 seconds then show modal
       setTimeout(() => {
-        $(cs_modal).modal('show');
-        localStorage.setItem('last_shown', today);
+        $(cs_modal).modal("show");
+        localStorage.setItem("last_shown", today);
       }, 5000);
-      console.log('showing coming soon');
+    } else {
+      const nt = new Date(parseInt(last_shown) + vph);
     }
-    else
-    {
-      console.log('not yet time', time_shown, vph);
-      const nt = new Date(parseInt(last_shown) + vph)
-      console.log('next_time', nt);
-    }
-
-
   }
 
   showComingSoon();
-})
+});
