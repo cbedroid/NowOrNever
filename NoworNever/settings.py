8455528e7746a90886d8b0a9475d4796afc9f44b
cbedroid <cbedroid1614@gmail.com>
2020-09-23 19:45:51 -0400

    Updated static root for heroku

--- NoworNever/settings.py
+++ NoworNever/settings.py
@@ -159,6 +159,7 @@ TIME_INPUT_FORMATS = [
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = "/media/"
